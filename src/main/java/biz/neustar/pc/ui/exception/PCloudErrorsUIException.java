5e16ab972856fe0c9c599ddd7fa7db0322723901
kvats <kapil.vats@impetus.co.in>
2015-06-25 12:59:33 +0530

    Added code to show client sepecific error messages using properties file.

--- src/main/java/biz/neustar/pc/ui/exception/PCloudErrorsUIException.java
+++ src/main/java/biz/neustar/pc/ui/exception/PCloudErrorsUIException.java
@@ -2,40 +2,49 @@ package biz.neustar.pc.ui.exception;
 
 import java.util.List;
 
+import biz.neustar.pc.ui.utils.PCloudErrorUtil;
 import biz.neustar.pcloud.rest.dto.PCloudError;
 
-public class PCloudErrorsUIException extends RuntimeException{
+public class PCloudErrorsUIException extends RuntimeException {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private List<PCloudError> errors;
-	private int statusCode;
-	public List<PCloudError> getErrors() {
-		return errors;
-	}
-	public void setErrors(List<PCloudError> errors) {
-		this.errors = errors;
-	}
-	public int getStatusCode() {
-		return statusCode;
-	}
-	public void setStatusCode(int statusCode) {
-		this.statusCode = statusCode;
-	}
-	public PCloudErrorsUIException(List<PCloudError> errors, int statusCode) {
-		super();
-		this.errors = errors;
-		this.statusCode = statusCode;
-	}
-	@Override
-	public String toString() {
-		return "PCloudErrorsUIException [errors=" + errors + ", statusCode="
-				+ statusCode + "]";
-	}
-	
-	
-	
+    private List<PCloudError> errors;
+    private int statusCode;
+
+    public List<PCloudError> getErrors() {
+        if (errors != null) {
+            for (PCloudError pCloudError : errors) {
+                pCloudError.setErrorMessage(PCloudErrorUtil.getProperty(Integer.toString(pCloudError.getErrorCode())));
+            }
+        }
+        return errors;
+    }
+
+    public void setErrors(List<PCloudError> errors) {
+        this.errors = errors;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public PCloudErrorsUIException(List<PCloudError> errors, int statusCode) {
+        super();
+        this.errors = errors;
+        this.statusCode = statusCode;
+    }
+
+    @Override
+    public String toString() {
+        return "PCloudErrorsUIException [errors=" + errors + ", statusCode=" + statusCode + "]";
+    }
+
 }
--- src/main/java/biz/neustar/pc/ui/exception/PCloudUIException.java
+++ src/main/java/biz/neustar/pc/ui/exception/PCloudUIException.java
@@ -1,53 +1,65 @@
 package biz.neustar.pc.ui.exception;
 
-public class PCloudUIException extends RuntimeException{
+import biz.neustar.pc.ui.utils.PCloudErrorUtil;
 
-	/**
+public class PCloudUIException extends RuntimeException {
+
+    /**
      * 
      */
     private static final long serialVersionUID = 1L;
     int errorCode;
     String errorMessage;
-	int statusCode;
-	
-	/**
+    int statusCode;
+
+    /**
      * @return the errorCode
      */
     public int getErrorCode() {
         return errorCode;
     }
+
     /**
-     * @param errorCode the errorCode to set
+     * @param errorCode
+     *            the errorCode to set
      */
     public void setErrorCode(int errorCode) {
         this.errorCode = errorCode;
     }
+
     public String getErrorMessage() {
-		return errorMessage;
-	}
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
-	public int getStatusCode() {
-		return statusCode;
-	}
-	public void setStatusCode(int statusCode) {
-		this.statusCode = statusCode;
-	}
-	public PCloudUIException(int errorCode, String errorMessage, int statusCode) {
-		super();
-		this.errorCode = errorCode;
-		this.errorMessage = errorMessage;
-		this.statusCode = statusCode;
-	}
-	public PCloudUIException(String errorMessage) {
-		super();
-		this.errorMessage = errorMessage;
-	}
-	@Override
-	public String toString() {
-		return "PCloudUIException [errorCode="+errorCode+",errorMessage=" + errorMessage
-				+ ", statusCode=" + statusCode + "]";
-	}
-	
+        return PCloudErrorUtil.getProperty(Integer.toString(errorCode));
+
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public PCloudUIException(int errorCode, String errorMessage, int statusCode) {
+        super();
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.statusCode = statusCode;
+    }
+
+    public PCloudUIException(String errorMessage) {
+        super();
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public String toString() {
+        return "PCloudUIException [errorCode=" + errorCode + ",errorMessage=" + errorMessage + ", statusCode="
+                + statusCode + "]";
+    }
+
 }
